b8a2c8cf0c02f37329bd6536e008975586815519
dabren4 <darrenxychen@gmail.com>
2024-03-23 21:18:54 -0700

    added filler project for project sec

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import './Projects.css';
 
 function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <div className="projects-container">
+      <div className="project">
+        <h3>Project 1</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <div className="project-links">
+          <a href="https://github.com" target="_blank" rel="noopener noreferrer">Code</a>
+          <a href="https://www.project1.com" target="_blank" rel="noopener noreferrer">Live Website</a>
+        </div>
+      </div>
+
+      <div className="project">
+        <h3>Project 2</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <div className="project-links">
+          <a href="https://github.com" target="_blank" rel="noopener noreferrer">Code</a>
+          <a href="https://www.project2.com" target="_blank" rel="noopener noreferrer">Live Website</a>
+        </div>
+      </div>
+
+      <div className="project">
+        <h3>Project 3</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <div className="project-links">
+          <a href="https://github.com" target="_blank" rel="noopener noreferrer">Code</a>
+          <a href="https://www.project3.com" target="_blank" rel="noopener noreferrer">Live Website</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
