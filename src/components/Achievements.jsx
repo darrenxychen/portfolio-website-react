d930767c23cfccd91944be40c38003e4598cfa13
dabren4 <darrenxychen@gmail.com>
2024-04-03 23:24:23 -0700

    edited container

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -4,8 +4,8 @@ import './Achievements.css';
 function Achievements() {
   return (
     <div className="achievements-container">
-      <div className="achievement">
-        <a href="https://fullyhacks-test.onrender.com/" target="_blank" rel="noopener noreferrer">
+      <a href="https://fullyhacks-test.onrender.com/" target="_blank" rel="noopener noreferrer" className="achievement">
+        <div>
           <h2>FullyHacks 2023 Winner</h2>
           <h3>AI/ML Winner</h3>
           <p>Won first place in FullyHacks for developing WikiGuess.</p>
@@ -13,8 +13,8 @@ function Achievements() {
           <div className="achievement-details">
             <h4>April 2023</h4>
           </div>
-        </a>
-      </div>
+        </div>
+      </a>
     </div>
   );
 }
