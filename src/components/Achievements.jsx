ca6de3bbdab36da0968436963d003ff55fa023a5
dabren4 <darrenxychen@gmail.com>
2024-04-03 23:10:37 -0700

    added achievement container + css

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import './Achievements.css'
+import React from 'react';
+import './Achievements.css';
 
 function Achievements() {
   return (
-    <div>Achievements</div>
-  )
+    <div className="achievements-container">
+      <div className="achievement">
+        <h2>FullyHacks 2023 AI/ML Winner</h2>
+        <h3>AI/ML Winner</h3>
+        <p>Won first place in FullyHacks for developing WikiGuess.</p>
+        <div className="achievement-details">
+          <h4>April 2023</h4>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Achievements
+export default Achievements;
