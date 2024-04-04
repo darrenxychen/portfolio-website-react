b1a6a1b0b65db115978aa7e2a07434a8451da6db
dabren4 <darrenxychen@gmail.com>
2024-04-03 23:17:04 -0700

    added fullyhacks website link

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -5,12 +5,15 @@ function Achievements() {
   return (
     <div className="achievements-container">
       <div className="achievement">
-        <h2>FullyHacks 2023 AI/ML Winner</h2>
-        <h3>AI/ML Winner</h3>
-        <p>Won first place in FullyHacks for developing WikiGuess.</p>
-        <div className="achievement-details">
-          <h4>April 2023</h4>
-        </div>
+        <a href="https://fullyhacks-test.onrender.com/" target="_blank" rel="noopener noreferrer">
+          <h2>FullyHacks 2023 Winner</h2>
+          <h3>AI/ML Winner</h3>
+          <p>Won first place in FullyHacks for developing WikiGuess.</p>
+          <p>(Link is slow)</p>
+          <div className="achievement-details">
+            <h4>April 2023</h4>
+          </div>
+        </a>
       </div>
     </div>
   );
