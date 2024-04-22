dc8ff976a83191a1d57747ef3b28390aacea33bc
dabren4 <darrenxychen@gmail.com>
2024-04-21 23:24:41 -0700

    added a contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import './Contact.css';
 
-function Contact() {
+const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <section className="contact-section">
+      <div className="contact-content">
+        <h3>Get in Touch</h3>
+        <p>Download my resume below or contant me to my right!</p>
+        <div className="social-links">
+          <a href="#">Resume</a>
+        </div>
+      </div>
+      <div className="contact-form">
+        <form>
+          <input type="text" placeholder="Name" className="form-input" />
+          <input type="email" placeholder="Email" className="form-input" />
+          <textarea rows="5" placeholder="Message" className="form-textarea"></textarea>
+          <button type="submit" className="submit-button">Submit</button>
+        </form>
+      </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
