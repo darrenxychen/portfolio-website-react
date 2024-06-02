17ae0bbea39961b22bc4fafccf15bc9ef42fbf48
dabren4 <darrenxychen@gmail.com>
2024-06-01 19:29:23 -0700

    changed achievement box similar to projects

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import './Achievements.css';
+import React from "react";
+import "./Achievements.css";
 
 function Achievements() {
   return (
     <div className="achievements-container">
-      <a href="https://fullyhacks-test.onrender.com/" target="_blank" rel="noopener noreferrer" className="achievement">
-        <div>
+      <a
+        href="https://fullyhacks-test.onrender.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="achievement"
+        style={{ backgroundImage: 'url(static/images/wikiguess.png)' }}
+      >
+        <div className="achievement-content">
           <h2>FullyHacks 2023 Winner</h2>
           <h3>AI/ML Winner</h3>
           <p>Won first place in FullyHacks for developing WikiGuess.</p>
