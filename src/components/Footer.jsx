8a150cd2a342f8f4c840e72c1519a7e497155372
darrenxychen <darrenxychen@gmail.com>
2025-04-09 19:39:49 -0700

    fixed resume button in header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; 2024 Darren Chen. All Rights Reserved.</p>
+      <p>&copy; 2025 Darren Chen. All Rights Reserved.</p>
     </footer>
   );
 }
